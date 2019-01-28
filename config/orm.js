0c6abec0f86a9eb38bb0e4959ae6612521028117
ZGermain <zack.germain@gmail.com>
2019-01-28 17:22:39 -0600

    fleshed out ORM.js

--- config/orm.js
+++ config/orm.js
@@ -3,9 +3,41 @@ const connection = require("../config/connection.js");
 
 //ORM - Creates an object based on SQL data
 const orm = {
-  selectAll: ()=>{},
-  insertOne: ()=>{},
-  updateOne: ()=>{}
+
+  selectAll: (tableInput, callback)=>{
+
+    const queryString = `SELECT * FROM ${tableInput};`;
+
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      callback(result);
+    })
+  },
+
+  insertOne: (table, column, value, callback)=>{
+
+    const queryString = `INSERT INTO ${table} (${column}) 
+    VALUES (?)`;
+
+    console.log(queryString);
+
+    connection.query(queryString, [value], function(err, result) {
+      if (err) throw err;
+    });
+
+    callback(result);
+  },
+
+  updateOne: (table, column, value, condition, callback)=>{
+    const queryString = `UPDATE ${table} SET ${column} = ${value} WHERE ${condition}`;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+    })
+
+    callback(result);
+  }
 };
 
 
