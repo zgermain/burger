6516e95606c9609f95c223a3614c0c5f450fe3c7
ZGermain <zack.germain@gmail.com>
2019-01-28 17:29:16 -0600

    finished orm references in burger model js

--- models/burger.js
+++ models/burger.js
@@ -4,6 +4,24 @@ const orm = require("../config/orm.js");
 //Burger Model
 const burger = {
 
+  all: (callback) => {
+    orm.selectAll("burgers", function(result){
+      callback(result)
+    })
+  },
+  
+  create: (column, value, callback)=>{
+    orm.insertOne("burgers", column, value, function(result) {
+      callback(result);
+    })
+  },
+
+  update: (column, value, condition, callback) =>{
+    orm.updateOne("burgers", column, value, condition, function(result){
+      callback(result);
+    });
+  }
+
 };
 
 //Export Burger
